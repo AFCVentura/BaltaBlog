005384265858a49f6a3f3f585d470604b8f6a1d7
Ventura <joao.ventura1@alunos.sc.senac.br>
2025-01-28 22:59:33 -0300

    feat: Sets the environment variable

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+
+namespace BaltaBlog;
+
+public class Program
+{
+    private static readonly string CONNECTION_STRING;
+
+    static Program()
+    {
+        // Configuração do appsettings.json e variáveis de ambiente
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory()) // Define a raiz do projeto
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        // Recuperando a string de conexão
+        CONNECTION_STRING = config["ConnectionStrings:CONNECTION_STRING"];
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("String de Conexão: " + CONNECTION_STRING);
+    }
+}
